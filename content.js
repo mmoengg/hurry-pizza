3375ad03a8e1b2726fbdbd5cb385e3b4c5d4d606
mmoengg <mmoengg@gmail.com>
2024-12-12 15:19:21 +0900

    design: css 수정

--- content.js
+++ content.js
@@ -1,22 +1,23 @@
 setInterval(() => {
-	const messageDiv = document.createElement('div');
-	messageDiv.innerText = '테스뜨';
-	messageDiv.style.position = 'fixed';
-	messageDiv.style.color = 'red';
-	messageDiv.style.width = '300px';
-	messageDiv.style.height = '300px';
-	messageDiv.style.fontSize = '24px';
-	messageDiv.style.top = '10px';
-	messageDiv.style.right = '10px';
-	messageDiv.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-	messageDiv.style.border = '1px solid #000';
-	messageDiv.style.padding = '10px';
-	messageDiv.style.zIndex = '9999'; // 다른 요소들 위에 표시되도록
+	const newMessage = `
+		<div id="messageDiv" class="container__message">
+			<div class="wrap__message">
+				<h1>허리 피자</h1>
+			</div>
+		</div>
+	`;
 
-	document.body.appendChild(messageDiv);
+	const div = document.createElement('div');
+	div.innerHTML = newMessage;
+	div.addEventListener('click', handleContentClick);
+	document.body.appendChild(div);
 
-	// 3초 후에 메시지를 제거
 	setTimeout(() => {
 		messageDiv.remove();
-	}, 1500);
-}, 2000);
+	}, 120000);
+}, 600000);
+
+const handleContentClick = () => {
+	const messageDiv = document.getElementById('messageDiv');
+	messageDiv.remove();
+};
--- sidebar.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('undockButton').addEventListener('click', () => {
-	// 현재 활성화된 창의 ID를 가져온 후 사이드바를 분리
-	whale.windows.getCurrent((currentWindow) => {
-		const parentWindowId = currentWindow.id;
-		whale.sidebarAction.undock(parentWindowId, () => {
-			console.log('사이드바가 분리되었습니다.');
-		});
-	});
-});
