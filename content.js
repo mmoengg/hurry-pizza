2f5efe70e604013e5743d7f39aab76662ca57f64
mmoengg <mmoengg@gmail.com>
2024-12-12 15:37:22 +0900

    design: css 수정

--- content.js
+++ content.js
@@ -2,22 +2,19 @@ setInterval(() => {
 	const newMessage = `
 		<div id="messageDiv" class="container__message">
 			<div class="wrap__message">
-				<h1>허리 피자</h1>
+				허리 피자
 			</div>
 		</div>
 	`;
 
 	const div = document.createElement('div');
 	div.innerHTML = newMessage;
-	div.addEventListener('click', handleContentClick);
+	div.addEventListener('click', () => {
+		div.remove(); // div를 클릭하면 div를 제거
+	});
 	document.body.appendChild(div);
 
 	setTimeout(() => {
 		messageDiv.remove();
 	}, 120000);
 }, 600000);
-
-const handleContentClick = () => {
-	const messageDiv = document.getElementById('messageDiv');
-	messageDiv.remove();
-};
